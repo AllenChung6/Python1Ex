e41093d2e51ebe7bec555b136529d9ff94660335
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-14 12:01:08 -0400

    Finished Python1Ex

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,4 @@
+fahrenheit = 32
+celsius = fahrenheit - 32
+Conversion = str(fahrenheit) + " degrees fahrenheit is equal to " + str(celsius) + " degree(s) celsius"
+print(Conversion)
---
+++ first_name_500_times.py
@@ -0,0 +1,2 @@
+first_name = "Allen"
+print(first_name * 500)
